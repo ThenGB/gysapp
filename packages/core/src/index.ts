a513041b3cf7fc0b04316d14ab900ced259bbf43
Gilbert <123263227+ThenGB@users.noreply.github.com>
2026-08-12 19:40:27 +0700

    refactor: remove obsolete e-GYS profile export

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -20,5 +20,4 @@ export * from './cache/blob-store';
 export * from './cache/chord-index';
 export * from './cache/chord-lazy-cache';
 export * from './cache/manifest-fetcher';
-export * from './egys/profile';
 export * from './util/sha256';
